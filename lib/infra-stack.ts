7b1c71d4f5c90138454da3e9ac1305ceb68ae50f
Sai Swarup Rath <saiswaruprath7@gmail.com>
2023-11-09 21:34:11 -0800

    Create infra-stack.ts

---
+++ lib/infra-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+
+    // create a table
+const demoTable = new dynamodb.Table(this, 'dynamodblogicalid', {
+  readCapacity: 3,
+  writeCapacity: 3,
+  partitionKey: {
+    name: 'customerid',
+    type: dynamodb.AttributeType.NUMBER,
+  },
+  tableName: 'newTable'
+});
+  }
+}
